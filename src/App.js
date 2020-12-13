ac99daf1b1f5b81bd60f4150e927f2cf429d6034
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2020-12-13 15:04:35 -0500

    loading working still working on displaying data from the api onto the page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route } from 'react-router-dom'
 import React, { useState, useEffect } from 'react';
+import { Dimmer, Loader } from 'semantic-ui-react'
 import StartshipPage from './components/StarshipPage';
 import Nav from './components/Nav';
 
 import './App.css';
+import { Container } from 'semantic-ui-react';
 
 function App() {
-  const [starships, setStarships] = useState([]);
+  const [starships, setStarships] = useState([]); // set to an empty array
   const [loading, setLoading] = useState(true);
 
   useEffect(()=> {
@@ -17,17 +19,28 @@ function App() {
     }
 
     fetchStarships();
+    setLoading(false);
 
   }, [])
-
+  //testing to make sure you are fetching the data
   console.log('----starships----', starships);
 
 
   return (
    <BrowserRouter>
-   <div>
    <Nav />
-   <Route exact path="/" component={StartshipPage} />
+   <div>
+     {/* Setting up loading using dimmer and loader */}
+   {loading ? (
+     <Dimmer active inverted>
+       <Loader inverted>Loading</Loader>
+     </Dimmer>
+   ) : (
+     
+     <Route exact path="/">
+       <StartshipPage data={starships}/>
+     </Route>
+   ) }
    </div>
    </BrowserRouter>
   );
--- src/components/StarshipPage.js
+++ src/components/StarshipPage.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Dimmer, Loader } from 'semantic-ui-react'
 
 function StarshipPage(){
+
+   
+
     return(
         <>
-        <h1>My star ship page</h1>
+        <h1>star ships</h1>
         </>
     )
 }
