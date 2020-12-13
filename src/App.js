b0b5d5ea662b4baa311909a19f8c6f260a284aa9
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2020-12-13 13:54:18 -0500

    recieving data from api in the console

--- src/App.js
+++ src/App.js
@@ -6,6 +6,23 @@ import Nav from './components/Nav';
 import './App.css';
 
 function App() {
+  const [starships, setStarships] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=> {
+    async function fetchStarships() {
+      let res = await fetch('https://swapi.dev/api/starships/?format=json');
+      let data = await res.json();
+      setStarships(data.results)
+    }
+
+    fetchStarships();
+
+  }, [])
+
+  console.log('----starships----', starships);
+
+
   return (
    <BrowserRouter>
    <div>
